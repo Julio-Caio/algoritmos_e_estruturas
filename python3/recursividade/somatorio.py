e035282b392cde5874f50e87abda5e0946d3c997
Julio-Caio <julio.caio@academico.ifpb.edu.br>
2024-10-20 16:14:25 -0300

    Funções e exercícios básicos de recursividade - 20/10/2024

--- python3/recursividade/somatorio.py
+++ python3/recursividade/somatorio.py
@@ -1,4 +1,4 @@
-def somatorio_simples(array: list):
+def somatorio_simples(array: list) -> int:
     """
     Função recursiva simples para calcular o somatório de uma lista.
     """
@@ -6,23 +6,25 @@ def somatorio_simples(array: list):
     if len(array) == 0:
         return 0
 
-    print(f'\n[ + ] Somando {array[0]} com somatorio({array[1:]})\n')
+    print(f'\n[ + ] Somando {array[0]} com somatorio_simples({array[1:]})\n')
 
-    ## Caso recursivo: somamos o primeiro elemento com o somatório do restante ##
+    ## Chamada recursiva: somamos o primeiro elemento com o somatório do restante ##
     return array[0] + somatorio_simples(array[1:])
 
-def somatorio_com_indices(array: list, indice = 0):
+
+def somatorio_com_indices(array: list, indice: int = 0) -> int:
     """
     Função recursiva usando índices para calcular o somatório de uma lista.
     """
     ## Caso base: se o índice for o último, retorna o valor nesse índice ##
-    if indice == len(array) - 1:
-        return array[indice]
+    if indice == len(array):
+        return 0
 
-    print(f'\n[ + ] Somando {array[indice]} com somatorio_com_indices(array, indice + 1)\n')
+    print(f'\n[ + ] Somando {array[indice]} com somatorio_com_indices({array}, {indice + 1})\n')
 
-    ## Chamada recursiva: soma o elemento atual com o próximo ##
+    ## Chamada recursiva: somamos o elemento atual com o próximo ##
     return array[indice] + somatorio_com_indices(array, indice + 1)
+
 
 if __name__ == "__main__":
     array1 = [1, 3, 5, 7]
@@ -31,5 +33,5 @@ if __name__ == "__main__":
     operacao1 = somatorio_com_indices(array1, 0)
     operacao2 = somatorio_simples(array1)
 
-    print("Somatório usando índices:", operacao1)
-    print("Somatório simples e direto:", operacao2)
+    print("\nSomatório usando índices:", operacao1)
+    print("\nSomatório simples e direto:", operacao2)
